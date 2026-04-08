28131f9a5e30aec3f16032d86acb47e49c967001
Jefye <jefinatindia@gmail.com>
2026-04-09 00:01:37 +0530

    Update pathfinding.h

--- pathfinding.h/pathfinding.h
+++ pathfinding.h/pathfinding.h
@@ -4,7 +4,15 @@
 
 bool moveRobotFire(int &robotX,int &robotY,intFireX,int fire y);
 #endif
-bool bool moveRobotTowardsFire(int &robotX, int &robotY, int fireX, int fireY) {
-=======
-vsdfgasfdadsf
->>>>>>> 403a854e3b47791cf15f4754e7684200fdbac101
+bool moveRobotTowardsFire(int &robotX, int &robotY, int fireX, int fireY) {
+ if(robotX == fireX && robotY == fireY){
+    return true;
+ }
+
+ if(robotx < firex) {
+    robotX++;
+ } else if (robotX > fireX){
+     robotX--;
+ } else if (robotY < fireY) {
+     robotY++;
+ } else f (robotY > fireY) {
