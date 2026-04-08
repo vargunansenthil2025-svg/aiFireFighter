f9db88f9d22e75856e092087afa3569227b21c09
Jefye <jefinatindia@gmail.com>
2026-04-08 23:48:22 +0530

    Added pathfinding function for robot movement

---
+++ pathfinding.h/pathfinding.h
@@ -0,0 +1,6 @@
+#ifdef PATHFINDING_H
+#define PATHFINDING_H
+
+bool moveRobotFire(int &robotX,int &robotY,intFireX,int fire y);
+#endif
+bool bool moveRobotTowardsFire(int &robotX, int &robotY, int fireX, int fireY) {
